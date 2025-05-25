3ec1f5827ca66b416853aed271cfb8e190752f46
Voidliner <165174684+Voidliner@users.noreply.github.com>
2025-05-25 16:03:44 -0700

    Delete logout.js

--- logout.js
+++
@@ -1,7 +0,0 @@
-const logoutBtn = document.getElementById("logoutBtn");
-
-    logoutBtn.style.cursor = "pointer"; // Make it look clickable
-
-    logoutBtn.addEventListener("click", function () {
-      window.location.href = "logout.php"; // Redirect to logout
-    });
